cd72487644904aad704360a814c4e09876568e12
Jake Esveld <thejakeesveld@gmail.com>
2019-11-14 14:56:20 -0700

    Add entry table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +10,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tired_rating = db.Column(db.Integer)
+    wake_rating = db.Column(db.Integer)
+    time_slept = db.Column(db.Integer)
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Entry {} {}>'.format(self.user_id, self.date)
